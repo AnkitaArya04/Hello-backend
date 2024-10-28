e2c70447210ef82c1ef13bcacb78e8208f804826
Ankita Arya <ankitaarya1207@gmail.com>
2024-10-28 22:50:03 +0530

    postman configuration

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+// // require('dotenv').config({path: './env'})
+// import dotenv from "dotenv"
+// import connectDB from "./db/index.js";
+
+// dotenv.config({
+//     path: './env'
+
+// })
+
+// connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!!", err);
+// })
+
+
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 
-dotenv.config({
-    path: './env'
+dotenv.config({ path: './env' });
 
-})
+// Initialize Express app
+const app = express();
 
+// MongoDB connection setup
 connectDB()
-.then(() => {
+  .then(() => {
+    console.log("MongoDB connected!");
+
+    // Start the server after a successful MongoDB connection
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!!", err);
-})
+      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MONGO db connection failed!!!", err);
+  });
+
 
 
 
